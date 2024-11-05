51cd91b9e3d09b21b1109c7cadebf4dc6c2802a2
sreInk <srinknok@gmail.com>
2024-11-05 15:14:30 +0530

    koi

---
+++ vechil.py
@@ -0,0 +1,9 @@
+class Vechil:
+    def __init__(self, max,milage):
+
+        self.max = max
+        self.milage = milage
+elon = Vechil(240,120)
+
+print("Max",elon.max)
+print("millage",elon.milage)                    
